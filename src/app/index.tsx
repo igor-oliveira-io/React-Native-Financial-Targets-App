cb0c392c76824427fdbb27fdf87de3a42eb8b802
Igor Oliveira <igorvinicius83@gmail.com>
2025-08-01 00:10:50 -0300

    Expo Router Example

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,12 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
+import { router } from "expo-router"
 
 export default function Index() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text> Olá Expo Router</Text>
+            <Text> Tela Inicial</Text>
+
+            <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
         </View>
     )
 } 
