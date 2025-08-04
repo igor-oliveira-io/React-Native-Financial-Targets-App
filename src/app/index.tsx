06ea37f737aa67d4611c2e92890ac99f826fd009
Igor Oliveira <igorvinicius83@gmail.com>
2025-08-03 23:56:05 -0300

    teste texto volta

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { router } from "expo-router"
 export default function Index() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text> Home</Text>
+            <Text> Página Inicial</Text>
 
             <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
         </View>
