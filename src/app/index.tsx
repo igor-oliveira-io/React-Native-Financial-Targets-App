1629bc1be362f0649cb33f7c7f4691b2100dd950
Igor Oliveira <igorvinicius83@gmail.com>
2025-08-07 21:39:59 -0300

    Iniciando estilizacao do projeto final

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,11 @@
-import { View, Text, Button } from "react-native";
-import { router } from "expo-router"
-import { fontFamily } from "@/theme/fontFamily";
+import { HomeHeader } from "@/components/HomeHeader";
+import { View, } from "react-native";
+
 
 export default function Index() {
     return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <Text style={{ fontFamily: fontFamily.bold, fontSize: 34 }}> Página Inicial</Text>
-
-            <Button title="Nova Meta" onPress={() => router.navigate("/target")} />
-
-            <Button title="Transacao" onPress={() => router.navigate("/transaction/5432")} />
-            <Button title="Progresso" onPress={() => router.navigate("/in-progress/5432")} />
+        <View style={{ flex: 1 }}>
+            <HomeHeader />
         </View>
     )
 } 
