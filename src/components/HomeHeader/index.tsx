879d5a87b6a60335dbbc2222fbd153bc7eb2a2f5
Igor Oliveira <igorvinicius83@gmail.com>
2025-08-07 21:45:29 -0300

    exportar theme de apenas um lugar

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,8 +1,14 @@
+import { View, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import { styles } from "./styles";
 import { colors } from "@/theme/colors";
 
+
 export function HomeHeader() {
-    return <LinearGradient colors={[colors.blue[500], colors.blue[800]]} style={styles.container} />
+    return <LinearGradient colors={[colors.blue[500], colors.blue[800]]} style={styles.container}>
+        <View>
+            <Text style={styles.label}>Saldo</Text>
+        </View>
+    </LinearGradient >
 }
--- src/components/HomeHeader/styles.ts
+++ src/components/HomeHeader/styles.ts
@@ -1,3 +1,4 @@
+import { colors, fontFamily } from "@/theme";
 import { StyleSheet } from "react-native";
 
 
@@ -5,5 +6,14 @@ export const styles = StyleSheet.create({
     container: {
         width: "100%",
         height: 324,
+        paddingHorizontal: 24,
+        justifyContent: "flex-end",
+        paddingBottom: 18,
+        gap: 24
+    },
+    label: {
+        fontSize: 12,
+        color: colors.white,
+        fontFamily: fontFamily.regular
     }
 })
